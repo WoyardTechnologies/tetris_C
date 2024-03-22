1096b0e2676b58a5b9e8fa913ff699e52e7a2f46
Wojtek Walczak <wojtek2walczak@gmail.com>
2024-03-22 13:18:52 +0100

    ide reformatted

--- tetris.c
+++ tetris.c
@@ -24,7 +24,7 @@ const struct point DOWN = {0, 1};
 const struct point LEFT = {-1, 0};
 const struct point RIGHT = {1, 0};
 
-const int DELAY = 75;
+const int DELAY = 100;
 const int RANDOM_SEED = 123456789;
 const int N_OF_TYPES = 7;
 const int N_OF_ORIENTATIONS = 4;
@@ -43,24 +43,24 @@ int game_matrix[GAME_MATRIX_WIDTH][GAME_MATRIX_HEIGHT] = {0};
 struct point piece;
 int piece_type, piece_orientation;
 
-void tetris_init(){
+void tetris_init() {
     float screen_aspect_ratio = SCREEN_WIDTH / (float) SCREEN_HEIGHT;
     float matrix_aspect_ratio = GAME_MATRIX_WIDTH / (float) GAME_MATRIX_HEIGHT;
-    if (screen_aspect_ratio > matrix_aspect_ratio){ // screen is wider than game matrix
+    if (screen_aspect_ratio > matrix_aspect_ratio) { // screen is wider than game matrix
         BOARD_HEIGHT = SCREEN_HEIGHT * BOARD_HEIGHT_SCALE;
         BOARD_WIDTH = BOARD_HEIGHT * matrix_aspect_ratio;
     } else { //screen is narrower or equal to game matrix
         BOARD_WIDTH = SCREEN_WIDTH * BOARD_WIDTH_SCALE;
         BOARD_HEIGHT = BOARD_WIDTH / matrix_aspect_ratio;
     }
-    ORIGIN.x = (SCREEN_WIDTH - BOARD_WIDTH)/2;
-    ORIGIN.y = (SCREEN_HEIGHT - BOARD_HEIGHT)/2;
+    ORIGIN.x = (SCREEN_WIDTH - BOARD_WIDTH) / 2;
+    ORIGIN.y = (SCREEN_HEIGHT - BOARD_HEIGHT) / 2;
     SQUARE_WIDTH = BOARD_WIDTH / GAME_MATRIX_WIDTH;
     SQUARE_HEIGHT = BOARD_HEIGHT / GAME_MATRIX_HEIGHT;
     srand(RANDOM_SEED);
 }
 
-void draw_square_in_matrix(int x, int y, enum color col, int filled){
+void draw_square_in_matrix(int x, int y, enum color col, int filled) {
     if (filled) {
         gfx_filledRect(ORIGIN.x + x * SQUARE_WIDTH, ORIGIN.y + y * SQUARE_HEIGHT,
                        ORIGIN.x + (x + 1) * SQUARE_WIDTH, ORIGIN.y + (y + 1) * SQUARE_HEIGHT,
@@ -72,10 +72,10 @@ void draw_square_in_matrix(int x, int y, enum color col, int filled){
     }
 }
 
-void draw_game_matrix(){
-    for (int i = 0; i < GAME_MATRIX_WIDTH; i++){
-        for (int j = 0; j < GAME_MATRIX_HEIGHT; j++){
-            if (game_matrix[i][j] != 0){
+void draw_game_matrix() {
+    for (int i = 0; i < GAME_MATRIX_WIDTH; i++) {
+        for (int j = 0; j < GAME_MATRIX_HEIGHT; j++) {
+            if (game_matrix[i][j] != 0) {
                 draw_square_in_matrix(i, j, YELLOW, 1);
             } else {
                 draw_square_in_matrix(i, j, WHITE, 0);
@@ -84,7 +84,7 @@ void draw_game_matrix(){
     }
 }
 
-void draw_falling_piece(){
+void draw_falling_piece() {
     for (int i = 0; i < SIZE_OF_PIECE; i++) {
         for (int j = 0; j < SIZE_OF_PIECE; j++) {
             if (pieces[piece_type][piece_orientation][i][j] == 1) {
@@ -96,16 +96,17 @@ void draw_falling_piece(){
     }
 }
 
-int check_collision(struct point dir_vector, int orientation){ // checks for collision in the direction of dir_vector
-    for (int i = 0; i < SIZE_OF_PIECE; i++){
-        for (int j = 0; j < SIZE_OF_PIECE; j++){
-            if (pieces[piece_type][orientation][i][j] != 0){
-                int new_x = piece.x + i + dir_vector.x;
-                int new_y = piece.y + j + dir_vector.y;
-                if (new_x < 0 || new_x >= GAME_MATRIX_WIDTH || new_y >= GAME_MATRIX_HEIGHT){
+int check_collision(struct point dir_vector, int orientation) { // checks for collision in the direction of dir_vector
+    int new_x, new_y;
+    for (int i = 0; i < SIZE_OF_PIECE; i++) {
+        for (int j = 0; j < SIZE_OF_PIECE; j++) {
+            if (pieces[piece_type][orientation][i][j] != 0) {
+                new_x = piece.x + i + dir_vector.x;
+                new_y = piece.y + j + dir_vector.y;
+                if (new_x < 0 || new_x >= GAME_MATRIX_WIDTH || new_y >= GAME_MATRIX_HEIGHT) {
                     return 1;
                 }
-                if (new_y >= 0 && game_matrix[new_x][new_y] != 0){
+                if (new_y >= 0 && game_matrix[new_x][new_y] != 0) {
                     return 1;
                 }
             }
@@ -114,17 +115,17 @@ int check_collision(struct point dir_vector, int orientation){ // checks for col
     return 0;
 }
 
-void append_piece_to_matrix(){
-    for (int i = 0; i < SIZE_OF_PIECE; i++){
-        for (int j = 0; j < SIZE_OF_PIECE; j++){
-            if (pieces[piece_type][piece_orientation][i][j] != 0){
+void append_piece_to_matrix() {
+    for (int i = 0; i < SIZE_OF_PIECE; i++) {
+        for (int j = 0; j < SIZE_OF_PIECE; j++) {
+            if (pieces[piece_type][piece_orientation][i][j] != 0) {
                 game_matrix[piece.x + i][piece.y + j] = 1;
             }
         }
     }
 }
 
-struct point get_center_offset(int type, int orientation){
+struct point get_center_offset(int type, int orientation) {
     struct point offset = {0, 0};
     for (int i = 0; i < SIZE_OF_PIECE; i++) {
         for (int j = 0; j < SIZE_OF_PIECE; j++) {
@@ -138,12 +139,12 @@ struct point get_center_offset(int type, int orientation){
     return offset;
 }
 
-int rotate_piece(){
+int rotate_piece() {
     int new_orientation = (piece_orientation + 1) % N_OF_ORIENTATIONS;
     struct point offset = get_center_offset(piece_type, new_orientation);
     struct point old_offset = get_center_offset(piece_type, piece_orientation);
     struct point move = {old_offset.x - offset.x, old_offset.y - offset.y};
-    if (!check_collision(move, new_orientation)){
+    if (!check_collision(move, new_orientation)) {
         piece_orientation = new_orientation;
         piece.x += move.x;
         piece.y += move.y;
@@ -152,31 +153,31 @@ int rotate_piece(){
     return 0;
 }
 
-void spawn_piece(){
+void spawn_piece() {
     piece.x = GAME_MATRIX_WIDTH / 2 - SIZE_OF_PIECE / 2;
     piece.y = 0;
     piece_type = random() % N_OF_TYPES;
     piece_orientation = random() % N_OF_ORIENTATIONS;
 }
 
-void drop_rows_above(int row){
-    for (int k = row; k > 0; k--){
-        for (int column = 0; column < GAME_MATRIX_WIDTH; column++){
-            game_matrix[column][k] = game_matrix[column][k-1];
+void drop_rows_above(int row) {
+    for (int k = row; k > 0; k--) {
+        for (int column = 0; column < GAME_MATRIX_WIDTH; column++) {
+            game_matrix[column][k] = game_matrix[column][k - 1];
         }
     }
 }
 
-int check_for_full_lines(){
-    for (int row = GAME_MATRIX_HEIGHT-1; row >= 0; row--){
+int check_for_full_lines() {
+    for (int row = GAME_MATRIX_HEIGHT - 1; row >= 0; row--) {
         int full = 1;
-        for (int column = 0; column < GAME_MATRIX_WIDTH; column++){
-            if (game_matrix[column][row] == 0){
+        for (int column = 0; column < GAME_MATRIX_WIDTH; column++) {
+            if (game_matrix[column][row] == 0) {
                 full = 0;
                 break;
             }
         }
-        if (full){
+        if (full) {
             drop_rows_above(row);
             return 1;
         }
@@ -184,18 +185,18 @@ int check_for_full_lines(){
     return 0;
 }
 
-void draw_background(){
-    gfx_filledRect(0, 0, SCREEN_WIDTH-1, SCREEN_HEIGHT-1, BLACK);
+void draw_background() {
+    gfx_filledRect(0, 0, SCREEN_WIDTH - 1, SCREEN_HEIGHT - 1, BLACK);
 }
 
-void display_game_over(){
+void display_game_over() {
     gfx_textout(ORIGIN.x + BOARD_WIDTH * GAME_OVER_X_OFFSET_SCALE,
                 ORIGIN.y + BOARD_HEIGHT * GAME_OVER_Y_OFFSET_SCALE,
                 "GAME OVER", RED);
 }
 
-int handle_input(){
-    switch(gfx_pollkey()){
+int handle_input() {
+    switch (gfx_pollkey()) {
         case SDLK_RIGHT:
             if (!check_collision(RIGHT, piece_orientation))
                 piece.x++;
@@ -211,16 +212,19 @@ int handle_input(){
         case SDLK_SPACE:
             rotate_piece();
             break;
+        case SDLK_UP:
+            rotate_piece();
+            break;
         case SDLK_ESCAPE:
             return EXIT;
         default:
-            if (!check_collision(DOWN, piece_orientation)){
+            if (!check_collision(DOWN, piece_orientation)) {
                 piece.y++;
             } else {
                 append_piece_to_matrix();
-                while(check_for_full_lines() == 1); // drop all full lines
+                while (check_for_full_lines()); // drop all full lines
                 spawn_piece();
-                if (check_collision(ZERO, piece_orientation)){
+                if (check_collision(ZERO, piece_orientation)) {
                     return GAME_OVER;
                 }
             }
@@ -244,5 +248,5 @@ int main() {
         gfx_updateScreen();
         SDL_Delay(DELAY);
     }
-    while(gfx_getkey() != SDLK_ESCAPE);
+    while (gfx_getkey() != SDLK_ESCAPE);
 }
